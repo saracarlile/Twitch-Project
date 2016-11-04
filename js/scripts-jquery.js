11a519c3a9eb61a3a4856dd933bc9980295389ad
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-04 17:03:51 -0500

    add links to user names

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
   var online;
 
-  $.each(userNames, function (index, value) {
+  $.each(userNames, function (index, value) {  //build DOM from API results sending userNames array
     $.getJSON('https://api.twitch.tv/kraken/streams/' + value + '?client_id=lwqoscrxiee347pi183ialxp5z7r8rq&callback=?', function (data) {
-      //   console.log(data);
+        console.log(data);
       if (data.hasOwnProperty('stream')) {
         data.stream === null ? online = "Offline" : online = "Online";
       }
@@ -13,18 +13,19 @@ $(document).ready(function () {
       }
 
       if (online === "Offline") {
-        $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p class="black">' + online + '</p></div></div></div>');
+        $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p><a href="https://www.twitch.tv/esl_sc2"' + value + '" target="blank"/a>' + value + '</p></div><div class="content-item"><p class="black">' + online + '</p></div></div></div>');
       }
       else if (online === "Online") {
-        $('.Grid').append('<div class="grid-items"><div class="mini-grid green-active"><div class="content-header"><p>' + value + '</p><br /><p><img src="' + data.stream.preview.small + '"/></div><div class="content-item"><p><span class="black">' + online + '</span><p>' + data.stream.game + '</p>' + '<p>' + data.stream.channel.status + '</p></div></div></div>');
+        $('.Grid').append('<div class="grid-items"><div class="mini-grid green-active"><div class="content-header"><p><a href="https://www.twitch.tv/esl_sc2"' + value +'" target="blank"/a>' + value + '</p><br /><p><img src="' + data.stream.preview.small + '"/></div><div class="content-item"><p><span class="black">' + online + '</span><p>' + data.stream.game + '</p>' + '<p>' + data.stream.channel.status + '</p></div></div></div>');
       }
       else if (online === "never") {
         $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p class="black"> does not exist ' + '</p></div></div></div>');
       }
-
     });
 
   });
+
+
 
 
 
