dd8f6ec57e93453a5774b8bdfe34c1e680da019b
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-04 10:20:01 -0500

    working with API output

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -1,32 +1,21 @@
 $(document).ready(function () {
   var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
-
+  var online;
   $.each(userNames , function (index, value){
   console.log(index + ':' + value); 
   $.getJSON('https://api.twitch.tv/kraken/streams/' + value + '?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function (data) {
     console.log(data);
-    $('.Grid').append('<div class="grid-items"><p>' + value + '</p></div>');
+    if(data.stream == null ){
+      online = "offline";
+    }
+    else{
+      online = "online";
+    }
+    $('.Grid').append('<div class="grid-items"><p>' + value +  '  ' + online + '</p></div>');
   });
 
 });
 
 
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function (data) {
-  //  console.log(data);
-  });
-  $.getJSON('https://api.twitch.tv/kraken/streams/ESL_SC2?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function (data) {
-  //  console.log(data);
-  });
-  $.getJSON('https://api.twitch.tv/kraken/streams/OgamingSC2?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function (data) {
-  //  console.log(data);
-  });
-  $.getJSON('https://api.twitch.tv/kraken/streams/cretetion?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function (data) {
- //   console.log(data);
-  });
-
-
-
-
-//$.getJSON('https://api.twitch.tv/kraken/streams/' + channel + '?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function(data) { ... });
 
 });
