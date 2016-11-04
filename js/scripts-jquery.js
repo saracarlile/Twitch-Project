4576f22fbd378437562158d02115b61ab657feaa
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-04 15:45:41 -0500

    styles

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
   var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
   var online;
+ 
+
+
+
+
+
   $.each(userNames, function (index, value) {
     $.getJSON('https://api.twitch.tv/kraken/streams/' + value + '?client_id=lwqoscrxiee347pi183ialxp5z7r8rq&callback=?', function (data) {
-      console.log(data);
+   //   console.log(data);
       if( data.hasOwnProperty('stream')){
-        data.stream  === null ? online = "offline" : online = "online";
+        data.stream  === null ? online = "Offline" : online = "Online";
       }
       else{
         online = "never";
@@ -15,11 +21,11 @@ $(document).ready(function () {
    //     console.log(data.stream.preview.medium);
   //      console.log(data.stream.channel.status);
     
-     if(online === "offline"){
-         $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p>' + online + '</p></div></div></div>');
+     if(online === "Offline"){
+         $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p class="black">' + online + '</p></div></div></div>');
      }
-     else if (online === "online"){
-        $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p>' + online +'<br/>Game: '+ data.stream.game + ' <br/>Status: ' + data.stream.channel.status +'</p></div></div></div>');
+     else if (online === "Online"){
+        $('.Grid').append('<div class="grid-items"><div class="mini-grid green-active"><div class="content-header"><p>' + value + '</p><br /><p><img src="' + data.stream.preview.small + '"/></div><div class="content-item"><p><span class="black">' + online +'</span><p>'+ data.stream.game + '</p>' + '<p>' + data.stream.channel.status +'</p></div></div></div>');
      }
      else if (online === "never") {
          $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p> does not exist ' +'</p></div></div></div>');
@@ -29,6 +35,14 @@ $(document).ready(function () {
 
   });
 
+
+$.each(userNames, function (index, value) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + value + '?client_id=lwqoscrxiee347pi183ialxp5z7r8rq&callback=?', function (data) {
+     console.log(index + ', ' + value);
+     console.log(data);
+    });
+
+  });
 
 
 });
