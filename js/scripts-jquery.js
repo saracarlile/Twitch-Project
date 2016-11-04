97a38890a94f0efcd97ea65c652a9bf0c6688ea1
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-04 13:52:47 -0500

    working with API output

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -1,20 +1,33 @@
 $(document).ready(function () {
   var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
   var online;
-  $.each(userNames , function (index, value){
-  console.log(index + ':' + value); 
-  $.getJSON('https://api.twitch.tv/kraken/streams/' + value + '?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function (data) {
-    console.log(data);
-    if(data.stream == null ){
-      online = "offline";
-    }
-    else{
-      online = "online";
-    }
-    $('.Grid').append('<div class="grid-items"><p>' + value +  '  ' + online + '</p></div>');
-  });
+  $.each(userNames, function (index, value) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + value + '?client_id=lwqoscrxiee347pi183ialxp5z7r8rq&callback=?', function (data) {
+      console.log(data);
+      if( data.hasOwnProperty('stream')){
+        data.stream  === null ? online = "offline" : online = "online";
+      }
+      else{
+        online = "never";
+      }
+     
+  //      console.log(data.stream.game);
+   //     console.log(data.stream.preview.medium);
+  //      console.log(data.stream.channel.status);
+    
+     if(online === "offline"){
+         $('.Grid').append('<div class="grid-items"><div class="mini-grid"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p>' + online + '</p></div></div></div>');
+     }
+     else if (online === "online"){
+        $('.Grid').append('<div class="grid-items"><p>' + value + '  ' + online +' '+ data.stream.game + '<br/>Status: ' + data.stream.channel.status +'</p></div>');
+     }
+     else if (online === "never") {
+         $('.Grid').append('<div class="grid-items"><p>' + value + ' does not exist ' +'</p></div>');
+     }
+   
+    });
 
-});
+  });
 
 
 
