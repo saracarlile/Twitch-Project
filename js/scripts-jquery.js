c43bec3e616de538bcac40ba53e8013d8eff5a71
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-08 16:43:14 -0600

    fix layout issue

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         $('.Grid').append('<div class="grid-items"><div class="mini-grid non-active"><div class="content-header"><p><a href="https://www.twitch.tv/' + value + '" target="blank" >' + value + '</a></p></div><div class="content-item"><p><i>' + online + '</i></p></div></div></div>');
       }
       else if (online === "Online") {
-        $('.Grid').append('<div class="grid-items"><div class="mini-grid green-active"><div class="content-header"><div class="content-header-wrap"><div><a href="https://www.twitch.tv/' + value + '" target="blank">' + value + '<a/></div></div><div><img src="' + data.stream.preview.small + '"/></div></div><div class="content-item"><p>' + data.stream.game + ': '  + data.stream.channel.status + '</p></div></div></div>');
+        $('.Grid').append('<div class="grid-items"><div class="mini-grid green-active"><div class="content-header"><div><a href="https://www.twitch.tv/' + value + '" target="blank">' + value + '</a></div><div><img src="' + data.stream.preview.small + '"/></div></div><div class="content-item"><p>' + data.stream.game + ': '  + data.stream.channel.status + '</p></div></div></div>');
       }
       else if (online === "never") {
         $('.Grid').append('<div class="grid-items"><div class="mini-grid non-active"><div class="content-header"><p>' + value + '</p></div><div class="content-item"><p> does not exist ' + '</p></div></div></div>');
